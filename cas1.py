a4ec69e78a54c9b56a85b0456b18b1d081fe8ac1
Meriem Tafraoui <120062908+Meriem-t11y@users.noreply.github.com>
2025-05-01 12:24:25 +0100

    Update cas1.py

--- cas1.py
+++ cas1.py
@@ -3,7 +3,9 @@ from datetime import datetime
 
 mutex = threading.Semaphore(1)
 lock_ecriture = threading.Semaphore(1)
+mutex2 = threading.Semaphore(1)       # protège des redacteurs
 
+global nb_lecteurs 
 def lecture(reader_id):
     print(f"Lecteur {reader_id} est entrain de lire  " + datetime.now())
     #lecture mel database
@@ -14,24 +16,28 @@ def ecriture(writer_id):
     #ecriture mel database
     print(f"Redacteur {writer_id} a fini d'écrire "+datetime.now())
 
-def lecteur(lect_id):
-    global nb_lecteur
+
+
+def lecteur(id):
+    mutex2.acquire()        
     mutex.acquire()
-    nb_lecteur +=1
-    if nb_lecteur==1 :
+    nb_lecteurs += 1
+    if nb_lecteurs == 1:
         lock_ecriture.acquire()
     mutex.release()
-    lecture(lect_id)
+    mutex2.release()
+
+    lecture(id)
+
     mutex.acquire()
-    nb_lecteur-=1
-    if nb_lecteur==0 :
+    nb_lecteurs -= 1
+    if nb_lecteurs == 0:
         lock_ecriture.release()
     mutex.release()
 
-def ecriture(ecrit_id):
-    mutex.acquire()
-    if(nb_lecteur==0):
-        lock_ecriture.acquire()
-    mutex.release()
-    ecriture(ecrit_id)
+def redacteur(id):
+    mutex2.acquire()        
+    lock_ecriture.acquire()
+    ecriture(id)
     lock_ecriture.release()
+    mutex2.release()

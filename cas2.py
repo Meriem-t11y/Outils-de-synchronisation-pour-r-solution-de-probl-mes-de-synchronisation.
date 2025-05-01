f4a4b9081347119ceb1a80677b162ff8fc79335e
InesHaddouche <ines.haddouche12@gmail.com>
2025-05-01 21:20:47 +0100

    Update cas2.py

--- cas2.py
+++ cas2.py
@@ -8,14 +8,16 @@ mutex = threading.Semaphore(1)              # protège readcount
 database_access = threading.Semaphore(1)    # accès exclusif
 readcount = 0  # nb lecteurs actifs
 
-def lecteur():
+def lecteur(cas):
     global readcount
     mutex.acquire()
     readcount += 1
     if readcount == 1:
         database_access.acquire()  # le premier lecteur bloque la base
     mutex.release()
-    value=lecture(connection)
+
+    value=lecture(connection, cas, readcount)
+
     mutex.acquire()
     readcount -= 1
     if readcount == 0:
@@ -25,8 +27,12 @@ def lecteur():
     return value
 
 
-def redacteur(num):
+def redacteur(num, cas):
     database_access.acquire()  # attend que les lecteurs finissent
-    ecriture(connection, num)
+
+    info=ecriture(connection, num, cas)
+
     database_access.release()
+    return info
+
 

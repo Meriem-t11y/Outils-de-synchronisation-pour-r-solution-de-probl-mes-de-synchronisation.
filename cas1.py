ce5ee3929023e6282a4d097db4fe352a762913b1
Meriem Tafraoui <120062908+Meriem-t11y@users.noreply.github.com>
2025-05-02 23:38:44 +0100

    Update cas1.py

--- cas1.py
+++ cas1.py
@@ -1,41 +1,36 @@
 import threading
 from ecriture_lecture import lecture, ecriture, connection_setup
 
-
 connection = connection_setup()
 mutex = threading.Semaphore(1)
 lock_ecriture = threading.Semaphore(1)
-mutex2 = threading.Semaphore(1)       # protège des redacteurs
-
+mutex2 = threading.Semaphore(1)
 nb_lecteurs = 0
 
 def lecteur(cas):
-    mutex2.acquire()
-    mutex.acquire()
     global nb_lecteurs
+    mutex.acquire()
     nb_lecteurs += 1
     if nb_lecteurs == 1:
         lock_ecriture.acquire()
     mutex.release()
-    mutex2.release()
 
-    info=lecture(connection, cas, nb_lecteurs)
+    info = lecture(connection, cas, nb_lecteurs)
 
     mutex.acquire()
     nb_lecteurs -= 1
     if nb_lecteurs == 0:
         lock_ecriture.release()
     mutex.release()
-
     return info
 
 def redacteur(num, cas):
     mutex2.acquire()
     lock_ecriture.acquire()
-
-    info=ecriture(connection, num, cas)
-
-    lock_ecriture.release()
     mutex2.release()
 
+    info = ecriture(connection, num, cas)
+
+    lock_ecriture.release()
     return info
+    

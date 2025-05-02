55dc62aea45eb47595825e32af41875f5a113bf5
Meriem Tafraoui <120062908+Meriem-t11y@users.noreply.github.com>
2025-05-02 12:40:32 +0100

    Update initial_window.py

--- initial_window.py
+++ initial_window.py
@@ -20,29 +20,30 @@ def execute_simulation():
     operation = operation_var.get()
 
     def worker():
-        for i in range(10):  # Change 5 to the number of iterations you want
-            if selected_case == "Case 1: Reader Priority":
-                if operation == "Write":
-                    info = cas1.redacteur(num, selected_case)
-                else:
-                    info = cas1.lecteur(selected_case)
 
-            elif selected_case == "Case 2: Conditional Reader Priority":
-                if operation == "Write":
-                    info = cas2.redacteur(num, selected_case)
-                else:
-                    info = cas2.lecteur(selected_case)
+        if selected_case == "Case 1: Reader Priority":
+            if operation == "Write":
+                info = cas1.redacteur(num, selected_case)
+            else:
+                info = cas1.lecteur(selected_case)
 
-            elif selected_case == "Case 3: Writer Priority":
-                print(operation)
-                if operation == "Write":
-                    info = cas3.redacteur(num, selected_case)
-                else:
-                    info = cas3.lecteur(selected_case)
+        elif selected_case == "Case 2: Conditional Reader Priority":
+            if operation == "Write":
+                info = cas2.redacteur(num, selected_case)
+            else:
+                info = cas2.lecteur(selected_case)
 
-            logs.insert(tk.END, f"Simulation {i + 1}...\n")
-            logs.insert(tk.END, info )
-            time.sleep(0.9)
+        elif selected_case == "Case 3: Writer Priority":
+            print(operation)
+            if operation == "Write":
+                info = cas3.redacteur(num, selected_case)
+            else:
+                info = cas3.lecteur(selected_case)
+
+        logs.insert(tk.END, f"\nSimulation...\n  ")
+        logs.insert(tk.END, info[0]["operation"])
+        logs.insert(tk.END, info[0]["value"])
+        
 
     threading.Thread(target=worker).start()
 
@@ -55,7 +56,7 @@ root.title("Readers-Writers Problem Simulation")
 tk.Label(root, text="Select Case:").grid(row=0, column=0, padx=10, pady=5)
 selected_value = tk.StringVar()
 selected_value.set("Case 1: Reader Priority")
-case_dropdown = ttk.Combobox(root, textvariable=selected_value,
+case_dropdown = ttk.Combobox(root, textvariable=selected_value,state="readonly",
                              values=["Case 1: Reader Priority", "Case 2: Conditional Reader Priority",
                                      "Case 3: Writer Priority"])
 case_dropdown.grid(row=0, column=1, padx=10, pady=5)
@@ -84,7 +85,7 @@ execute_button.grid(row=4, column=0, columnspan=2, pady=10)
 # Logs output
 tk.Label(root, text="Simulation Logs:").grid(row=5, column=0, columnspan=2, pady=5)
 logs = tk.Text(root, height=10, width=50)
-logs.grid(row=6, column=0, columnspan=2, padx=10, pady=5)
+logs.grid(row=6, column=0, columnspan=2, padx=100, pady=50)
 
 # Run the application
 root.mainloop()

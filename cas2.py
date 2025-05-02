c4c2e78dec2440c5d81f0f417b105655959a7712
Meriem Tafraoui <120062908+Meriem-t11y@users.noreply.github.com>
2025-05-02 23:39:19 +0100

    Update cas2.py

--- cas2.py
+++ cas2.py
@@ -1,38 +1,30 @@
-import sqlite3
 import threading
-from ecriture_lecture import lecture, ecriture, connection_setup 
-
+from ecriture_lecture import lecture, ecriture, connection_setup
 
 connection = connection_setup()
-mutex = threading.Semaphore(1)              # protège readcount
-database_access = threading.Semaphore(1)    # accès exclusif
-readcount = 0  # nb lecteurs actifs
+mutex = threading.Semaphore(1)
+database_access = threading.Semaphore(1)
+readcount = 0
 
 def lecteur(cas):
     global readcount
     mutex.acquire()
     readcount += 1
     if readcount == 1:
-        database_access.acquire()  # le premier lecteur bloque la base
+        database_access.acquire()
     mutex.release()
 
-    value=lecture(connection, cas, readcount)
+    value = lecture(connection, cas, readcount)
 
     mutex.acquire()
     readcount -= 1
     if readcount == 0:
-        database_access.release()  # le dernier lecteur libère la base
+        database_access.release()
     mutex.release()
-
     return value
 
-
 def redacteur(num, cas):
-    database_access.acquire()  # attend que les lecteurs finissent
-
-    info=ecriture(connection, num, cas)
-
+    database_access.acquire()
+    info = ecriture(connection, num, cas)
     database_access.release()
     return info
-
-

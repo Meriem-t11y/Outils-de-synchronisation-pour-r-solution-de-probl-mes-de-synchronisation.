27d21a331cb2d99c5ec46ca0dfe681ab84b6b97a
Meriem Tafraoui <120062908+Meriem-t11y@users.noreply.github.com>
2025-05-02 23:39:00 +0100

    Update cas3.py

--- cas3.py
+++ cas3.py
@@ -1,42 +1,35 @@
 import threading
 from ecriture_lecture import lecture, ecriture, connection_setup
 
-
 connection = connection_setup()
-# Sémaphores
-mutex = threading.Semaphore(1)              # protège readcount
-read_try = threading.Semaphore(1)           # priorité rédacteurs
-database_access = threading.Semaphore(1)    # accès exclusif à la BDD
-
-readcount = 0  # compteur des lecteurs
+mutex = threading.Semaphore(1)
+read_try = threading.Semaphore(1)
+database_access = threading.Semaphore(1)
+readcount = 0
 
 def lecteur(cas):
     global readcount
-    read_try.acquire()  #p(read_try)
-    mutex.acquire()    #p(mutex)
+    read_try.acquire()
+    mutex.acquire()
     readcount += 1
     if readcount == 1:
-        database_access.acquire() #p(database_access)
-    mutex.release() #v(mutex)
-    read_try.release() #v(release)
+        database_access.acquire()
+    mutex.release()
+    read_try.release()
 
     value = lecture(connection, cas, readcount)
 
-    mutex.acquire() #p(mutex)
+    mutex.acquire()
     readcount -= 1
     if readcount == 0:
-        database_access.release()  # dernier lecteur libere la BDD
-    mutex.release() #v(mutex)
-
+        database_access.release()
+    mutex.release()
     return value
 
 def redacteur(num, cas):
-    read_try.acquire()         # p(read_try)
-    database_access.acquire()  # p(databse_access) pour acces exclusif
-
+    read_try.acquire()
+    database_access.acquire()
     info = ecriture(connection, num, cas)
-
-    database_access.release()  #v(database)
-    read_try.release()  #v(read_try)
-
+    database_access.release()
+    read_try.release()
     return info
